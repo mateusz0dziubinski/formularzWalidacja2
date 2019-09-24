e61860b1584ac3492960cabe045d16d0ad5cd214
mateusz0dziubinski <mateusz0dziubinski@o2.pl>
2019-09-24 16:56:53 +0200

    Zadanie: zrobione

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function mojaFunkcja(){
+    var mobile = document.getElementById("phone").value;
+    mobile.length;
+    if(mobile.length <= 8 || mobile.length >=10){
+        alert ('Wprowadzono nieprawidłowy numer');
+    }
+}
